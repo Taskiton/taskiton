8164d2c8c77af29c369c741041b4f33b1ea034e1
Arsh <arshdeepgary@gmail.com>
2020-01-22 22:02:32 -0800

    Updates

---
+++ precache-manifest.051d4d3713cc5fb2a89283ba1afc8f3c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83fd28c21fdb71d6f3beabeaa9027696",
+    "url": "/taskiton/index.html"
+  },
+  {
+    "revision": "e625168bbe95e430b501",
+    "url": "/taskiton/static/css/main.7e34f606.chunk.css"
+  },
+  {
+    "revision": "bf02015fa9d5d28bf685",
+    "url": "/taskiton/static/js/2.46e641a3.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/taskiton/static/js/2.46e641a3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e625168bbe95e430b501",
+    "url": "/taskiton/static/js/main.a85dc544.chunk.js"
+  },
+  {
+    "revision": "1d1c5a4e15af40c91fa9",
+    "url": "/taskiton/static/js/runtime-main.96836dd4.js"
+  }
+]);
