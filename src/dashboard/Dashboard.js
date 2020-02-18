2ab651dcfa304629d7c684837bd2efc2226d6b8d
Arsh <arshdeepgary@gmail.com>
2020-02-17 19:20:48 -0800

    created new grid for chat and announcement

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import Kanban from './Kanban';
-
+import './dashboard.css';
 import { makeStyles } from '@material-ui/core/styles';
-import Hidden from '@material-ui/core/Hidden';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(theme => ({
-    mainGrids: {
-        width: "32vw",
-        height: "90vh",
+    gridTeam: {
+        backgroundColor:"blue",
+        width:"5vw",
+        height:"91vh",        
         [theme.breakpoints.down('xs')]: {
-            width: "100vw",
+            display:"none",
+        }
+    },
+    gridKanban: {
+        backgroundColor:"red", 
+        width:"65vw",
+        height:"91vh",
+        overflow: "auto",
+        [theme.breakpoints.down('xs')]: {
+            width:"100vw",
+        }
+    },
+    gridChat: {
+        backgroundColor:"yellow", 
+        width:"30vw",
+        height:"91vh",
+        [theme.breakpoints.down('xs')]: {
+            display:"none",
         }
     }
+
 }));
 
 export default function Dashboard() {
@@ -20,8 +38,16 @@ export default function Dashboard() {
     const classes = useStyles();
 
     return (
-        <div>
-            <Kanban/>
+        <div className={classes.root}>
+            <Grid container spacing={0}>
+                <Grid item className={classes.gridTeam}>
+                </Grid>
+                <Grid item className={classes.gridKanban}>
+                    <Kanban/>
+                </Grid>
+                <Grid item className={classes.gridChat}>
+                </Grid>
+            </Grid>
         </div>
     );
 }
