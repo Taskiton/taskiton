f2c773414cf79a423b5e28671f3fce95d5f9ebbe
Arsh <arshdeepgary@gmail.com>
2020-03-16 22:50:36 -0700

    dynamic side user list

--- src/dashboard/TeamList/TeamList.js
+++ src/dashboard/TeamList/TeamList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './teamList.css';
 import Avatar from '@material-ui/core/Avatar';
 import { makeStyles } from '@material-ui/core/styles';
@@ -24,13 +24,34 @@ export default function TeamList() {
 
     let classes = useStyles();
 
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        let url = "http://api.taskiton.wmdd.ca/userlist";
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json, text/plain, */*'
+                , 'Content-Type': 'application/json'
+            }
+        }).then(response => {
+            if (response.status >= 400) {
+                alert("Error - refresh page and try again");
+            }
+            return response.json();
+        })
+            .then(data => {
+                //console.log(data);
+                setUsers(data);
+            }).catch(function (err) {
+                alert("Error - refresh page and try again");
+                console.log(err)
+            });
+    }, [])
+
     return(
         <div>
-            <Avatar className={classes.avatar}>YT</Avatar>
-            <Avatar className={classes.avatar}>AU</Avatar>
-            <Avatar className={classes.avatar}>AU</Avatar>
-            <Avatar className={classes.avatar}>AU</Avatar>
-            <Avatar className={classes.avatar}>AU</Avatar>
+            {users.map(user => <Avatar className={classes.avatar} key={user.user_id}>{(user.name).split(" ")[0][0]}{(user.name).split(" ")[1][0]}</Avatar>
+                    )}
         </div>
     );
 }
